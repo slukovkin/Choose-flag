c2209b8d920bdb5170a34d59ed8a7e06dfbf9192
Sergey Lukovkin <slukovkin@icloud.com>
2024-02-16 19:31:35 +0200

    Initial Commit

---
+++ Choose Flags/Choose_FlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Choose_FlagsApp.swift
+//  Choose Flags
+//
+//  Created by Sergey Lukovkin on 16.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Choose_FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
